ac1784c516402db23692137f1d27ce062dc11f23
Documenter.jl <documenter@juliadocs.github.io>
2023-01-31 14:48:11 +0000

    build based on bed1a1c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NamedTrajectories","category":"page"},{"location":"#NamedTrajectories","page":"Home","title":"NamedTrajectories","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NamedTrajectories.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NamedTrajectories]","category":"page"}]
+}
