bc87d60529379401c984c4955127e34708c21436
Andy Goldschmidt <agoldschmidt11@gmail.com>
2025-02-24 10:02:36 -0600

    fix: docs

--- docs/literate/plotting.jl
+++ docs/literate/plotting.jl
@@ -7,19 +7,15 @@
 # Visualizing trajectories is crucial for understanding the solutions of trajectory optmization problems and `NamedTrajectories` exports a `plot` function that contains a lot of functionality that is continually being added to. [Makie.jl](https://docs.makie.org/stable/) is used as the plotting framework, and at the moment the default backend is `CairoMakie`, as it creates high quality vector graphics. The function is called as follows:
 
 #=
-```@docs
-NamedTrajectories.plot(
-    ::NamedTrajectory,
-    ::Union{Symbol, Vector{Symbol}, Tuple{Vararg{Symbol}}} = traj.names;
-    kwargs...
-)
-```
+# ```@docs; canonical = false
+# NamedTrajectories.trajectoryplot
+# ```
 =#
 
 # ## Basic example
 
-# Let's first create a simple trajectory to plot
-
+# Use a Makie backend to automatically load the NamedTrajectories plotting extension
+using CairoMakie
 using NamedTrajectories
 
 ## define the number timestamps
@@ -55,13 +51,17 @@ traj = NamedTrajectory(
 )
 
 ## plot the trajectory
-plot(traj)
+trajectoryplot(traj)
 
 # ## Selectively plotting components
 
-# We can selectively plot components of the trajectory by passing a `Vector` of `Symbol`s to the `components` keyword argument. For example, if we only wanted to plot the state and the first control we could do the following:
+#=
+We can selectively plot components of the trajectory by passing a `Vector` of `Symbol`s to 
+the `components` keyword argument. For example, if we only wanted to plot the state and the 
+first control we could do the following:
+=#
 
-plot(traj, [:x, :u])
+trajectoryplot(traj, [:x, :u])
 
 # ## Playing with transformations
 
@@ -69,42 +69,17 @@ plot(traj, [:x, :u])
 
 # For example, if we wanted to plot absolute values of the states we could do the following:
 
-transformations = OrderedDict(
-    :x => x -> abs.(x),
-)
+transformations = [(:x, x -> abs.(x))]
 
-plot(traj, [:x]; transformations=transformations)
+trajectoryplot(traj, [:x]; transformations=transformations)
 
 # We can also pass multiple transformations to the same component, with selective labels and titles:
 
 ## define the transformations
-transformations = OrderedDict(
-    :x => [
-        x -> [x[1] + x[2], x[3] - x[2]],
-        x -> [x[1] - x[2], x[3] + x[2]]
-    ],
-)
-
-## define the labels
-transformation_labels = OrderedDict(
-    :x => [
-        "\\tilde{x}",
-        "\\hat{x}"
-    ],
-)
-
-## define the titles
-transformation_titles = OrderedDict(
-    :x => [
-        L"x_1 + x_2, x_3 - x_2",
-        L"x_1 - x_2, x_3 + x_2"
-    ],
-)
+transformations = [
+    (:x, x -> [x[1] + x[2], x[3] - x[2]]),
+    (:x, x -> [x[1] - x[2], x[3] + x[2]])
+]
 
 ## plot the trajectory, with only the transformation and the `u` control
-plot(traj, [:u];
-    transformations=transformations,
-    transformation_labels=transformation_labels,
-    include_transformation_labels=[[true, true]],
-    transformation_titles=transformation_titles,
-)
+trajectoryplot(traj, [:u]; transformations=transformations,)
